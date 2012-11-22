7ba8182c2e63644f19a78e11b55c44c93481c16e
Richárd Ötvös <otvos.richard@gmail.com>
2012-11-22 01:02:58 +0100

    Added the logic for the start screen.

--- src/game.js
+++ src/game.js
@@ -3,10 +3,36 @@ var TicTacToe = function(){
     //we have three states: START, PLAYING, END
     var gameState = "START";
 
+    var $gameBoard = $('#board');
+
+    //display start screen
+    function initStartScreen(){
+
+        //formats the board to the start screen
+        $gameBoard.addClass('startScreen');
+
+        //adds the game title
+        $('<h1>Tic-Tac-Toe</h1>').addClass('gameTitle')
+            .appendTo($gameBoard);
+
+        //adds the Play button
+        $('<div><button type="button">Play!</button></div>').addClass('playButton')
+            .appendTo($gameBoard)
+            .click(function(){
+                gameState = 'PLAYING';
+                updateDisplay();
+            });
+
+
+    }
+
     //updates the game display
     function updateDisplay(){
         if (gameState === "START"){
-            console.log('start');
+            initStartScreen();
+        }
+        else if (gameState === "PLAYING"){
+            console.log('playing');
         }
     }
 
